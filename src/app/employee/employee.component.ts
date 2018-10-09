5a31e58efbf93a79c6a72352a40d5133e25371ab
Prashant Rajoriya <prashant.rajoriya21@gmail.com>
2018-10-09 06:58:15 +0530

    Modidy folder structure and Updated employee Service

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,15 +1,22 @@
+import { EmployeeService } from './../shared/employee.service';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { EmployeeService } from '../shared/employee.service';
+import { Employee } from '../shared/employee.model';
 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.css']
+  styleUrls: ['./employee.component.css'],
+  providers: [EmployeeService]
 })
 export class EmployeeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService) { }
 
   ngOnInit() {
   }
+
 
 }
---
+++ src/app/shared/employee.model.ts
@@ -0,0 +1,10 @@
+export class Employee {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  employeeCode: number;
+  img: string;
+  phone: number;
+  email: string;
+}
+
